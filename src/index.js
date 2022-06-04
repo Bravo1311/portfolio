7e1be4b3cba1c03dfd5b73f3807ffe262d82f159
Kartik <agrawalkartik1999@gmail.com>
2022-06-05 01:05:38 +0530

    new version

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <App />
   </React.StrictMode>
 );
